ba193a677217bbc5c39a4d84b0f443dc693049a9
Humam <humam.abedrabbo@hotmail.com>
2020-02-27 05:37:56 +0200

    Add user interface styling

---
+++ AMS/Controllers/CoreUIController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AMS.Controllers
+{
+    [Route("CoreUI")]
+    public class CoreUIController : Controller
+    {
+        [Route("{view=Index}")]
+        public IActionResult Index(string view)
+        {
+            ViewData["Title"] = "CoreUI Free Bootstrap Admin Template";
+
+            return View(view);
+        }
+    }
+}
