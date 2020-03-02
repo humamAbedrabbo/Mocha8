6d50afa11909559ac113a0c1d03580b23b3cf241
Humam <humam.abedrabbo@hotmail.com>
2020-03-02 20:24:15 +0200

    Fix Bar Chart bug Tickets per location

--- AMS/wwwroot/js/charts.js
+++ AMS/wwwroot/js/charts.js
@@ -76,7 +76,7 @@ $.getJSON('/Home/GetLocationTicketsData', {}, function (data) {
                 highlightStroke: 'rgba(220, 220, 220, 1)',
                 data: chartTicketsByLocationModel.dataSetLabels[0].data
             }, {
-                label: chartMonthlyTicketsModel.dataSetLabels[1].label,
+                    label: chartTicketsByLocationModel.dataSetLabels[1].label,
                 backgroundColor: 'rgba(151, 187, 205, 0.5)',
                 borderColor: 'rgba(151, 187, 205, 0.8)',
                 highlightFill: 'rgba(151, 187, 205, 0.75)',
