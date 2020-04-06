febad6d07ce05fd39ba0b43bbdc197e477276aad
Humam <humam.abedrabbo@hotmail.com>
2020-04-06 21:56:27 +0300

    Configure areas in startup

--- AMS/Startup.cs
+++ AMS/Startup.cs
@@ -104,6 +104,9 @@ namespace AMS
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
+                    name: "areas",
+                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
